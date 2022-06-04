7d2c330851720ffb29a5e610bd2314f055359222
Mark Thorne <thorne.mark@gmail.com>
2022-06-04 00:01:45 -0500

    Initial starter code for minesweeper game

---
+++ main.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+root = Tk()
+
+# Override the default window settings
+root.configure(bg="burlywood")
+root.geometry('1200x600')
+root.resizable(False, False)
+root.title('Amazing Stuff!')
+
+top_frame = Frame(root, bg="sea green", width=1200, height=150)
+top_frame.place(x=0, y=0)
+
+
+# Run the game window
+root.mainloop()
