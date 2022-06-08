c973ebe48a8ec8fc42167a309892e79a11ac0d86
Mark Thorne <thorne.mark@gmail.com>
2022-06-07 23:40:11 -0500

    Added utils.py and created center mine cells frame

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from turtle import left
 import settings
+from utils import *
 
 root = Tk()
 
@@ -11,16 +12,16 @@ root.resizable(False, False)
 root.title('Amazing Stuff!')
 
 # Top bar
-top_frame = Frame(root, bg="sea green", width=1200, height=150)
+top_frame = Frame(root, bg="grey10", width=width_pct(100), height=height_pct(25))
 top_frame.place(x=0, y=0)   # Start the top frame from the upper left corner
 
-# Practice button using Frame()
-# mid_frame = Frame(root, bg="gold2", width=40, height=40)
-# mid_frame.place(x=580, y=50)
-
 # Left sidebar
-left_frame = Frame(root, bg="blue", width=300, height=450)
-left_frame.place(x=0, y=150)
+left_frame = Frame(root, bg="grey10", width=width_pct(25), height=height_pct(75))
+left_frame.place(x=0, y=height_pct(25))
+
+# Center frame
+center_frame = Frame(root, bg="grey10", width=width_pct(75), height=height_pct(75))
+center_frame.place(x=width_pct(25), y=height_pct(25))
 
 # Run the game window
 root.mainloop()
---
+++ utils.py
@@ -0,0 +1,7 @@
+import settings
+
+def height_pct(pct):
+  return (settings.HEIGHT / 100) * pct
+
+def width_pct(pct):
+  return (settings.WIDTH / 100) * pct
