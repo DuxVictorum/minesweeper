ab3eecaa358f5482b9eabb9507f58bc3306ea8f7
Mark Thorne <thorne.mark@gmail.com>
2022-06-09 23:59:02 -0500

    Created basic grid of cell buttons

--- cell.py
+++ cell.py
@@ -6,5 +6,5 @@ class Cell:
     self.cell_btn_object = None
 
   def create_btn_object(self, location):
-    btn = Button(location, text='Wham')
+    btn = Button(location, text='???')
     self.cell_btn_object = btn
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from cell import Cell
 root = Tk()
 
 # Override the default window settings
-root.configure(bg="burlywood")
+root.configure(bg="grey10")
 root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
 root.resizable(False, False)
 root.title('Amazing Stuff!')
@@ -24,10 +24,22 @@ left_frame.place(x=0, y=height_pct(25))
 center_frame = Frame(root, bg="grey10", width=width_pct(75), height=height_pct(75))
 center_frame.place(x=width_pct(25), y=height_pct(25))
 
-# Cells to click on
-c1 = Cell()
-c1.create_btn_object(center_frame)
-c1.cell_btn_object.place(x=0, y=0)
+# Cells to click on - sample only
+# c1 = Cell()
+# c1.create_btn_object(center_frame)
+# c1.cell_btn_object.grid(column=0, row=0)
+
+# c2 = Cell()
+# c2.create_btn_object(center_frame)
+# c2.cell_btn_object.grid(column=20, row=0)
+
+# Populate basic grid
+for x in range(5):
+  for y in range(5):
+    c = Cell()
+    c.create_btn_object(center_frame)
+    c.cell_btn_object.grid(column=x, row=y)
+
 
 # Run the game window
 root.mainloop()
