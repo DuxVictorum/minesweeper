d7d7d5b3dd830329240828b30b7550fb9bfe15a3
Mark Thorne <thorne.mark@gmail.com>
2022-06-13 23:54:17 -0500

    Started work on click results algorithm

--- cell.py
+++ cell.py
@@ -15,23 +15,33 @@ class Cell:
     Cell.all.append(self)
 
   def create_btn_object(self, location):
-    btn = Button(location, width=8, height=3, text="M")
+    btn = Button(location, width=8, height=3, text="???")
     btn.bind('<Button-1>', self.left_click_actions)
     btn.bind('<Button-3>', self.right_click_actions)
     self.cell_btn_object = btn
 
   def left_click_actions(self, event):
-    print(event)
-    print("I was left-clicked!")
+    if self.is_mine:
+      self.show_mine()
+    else:
+      self.show_cell()
 
-  def right_click_actions(self, event):
-    print(event)
-    print("Righty tighties!")
+  def show_mine(self):
+    # Interrupt game and display "You Lost" message
+    self.cell_btn_object.configure(bg='red', text='Boom!')
+
+  def show_cell(self):
+    pass
+
+def right_click_actions(self, event):
+    if self.is_mine:
+      self.show_mine()
+
 
   # Seed the mines
   @staticmethod
   def randomize_mines():
-    mined_cells = random.sample(Cell.all, MINES_COUNT)
+    mined_cells = random.sample(Cell.all, settings.MINES_COUNT)
     for mine in mined_cells:
       mine.is_mine = True
 
